2f25075aa356e7f398f3fcd7720a876a13354ac2
GWangY <63031568+GWangY@users.noreply.github.com>
2020-08-23 10:58:26 +0800

    vue学习--第三部分（模块化开发的学习）

---
+++ webpack的使用/01webpack/src/main.js
@@ -0,0 +1,13 @@
+
+require('./css/normal.css');
+require('./css/special.less');
+document.writeln('hshshhshhhhhhhhhhhhh')
+import Vue from 'vue'
+import App from './vue/App'
+new Vue({
+  el:"#app",
+  template:'<App/>',
+  component:{
+    App
+  }
+})
---
+++ webpack的使用/01webpack/webpack.config.js
@@ -0,0 +1,53 @@
+const path=require('path')
+module.exports={
+  entry:'./src/main.js',
+  output:{
+    path:path.join(__dirname,'dist'),
+    filename:'bundle.js'
+  },
+  /*publicPath:'dist/',*/
+
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test:/\.less$/,
+        use:[
+          {loader:'style-loader'},
+          {loader:'css-loader'},
+          {loader:'less-loader'}
+          ]
+      },
+      {
+        test:/\.(png|jpg|gif|jpeg)$/,
+        use:[
+            {
+          loader: 'url-loader',
+          options:{
+            limit:13000
+          }
+        }]
+      },
+      {
+        test:/\.js$/,
+        exclude:/(node_modules|bower_components)/,
+        use:{
+          loader:'babel-loader',
+          /*options:{
+            preset:['es2015']
+          }*/
+        }
+      },
+      {
+        test:/\.vue$/,
+        use:['vue-loader']
+      },
+    ]
+  },
+  resolve:{
+    alias:{"vue$":'vue/dist/vue.esm.js'}
+  }
+}
